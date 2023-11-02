3d8a68b43d35de9c08f61bc93c809971c470fc34
Andy Damevin <ia3andy@gmail.com>
2023-11-02 15:28:40 +0100

    Rename to locker-maven-plugin

--- src/main/java/io/mvnpm/maven/locker/LockerConstants.java
+++ src/main/java/io/mvnpm/maven/locker/LockerConstants.java
@@ -6,10 +6,10 @@ import java.util.regex.Pattern;
 public interface LockerConstants {
 
     Predicate<String> LOCK_GOAL_PREDICATE = Pattern.compile(
-            "^(io\\.mvnpm:)?mvnpm-locker(-maven-plugin)?(:[^:]+)?:lock$").asMatchPredicate();
-    String LOCKER_DIR = ".mvnpm-locker";
+            "^(io\\.mvnpm:)?locker(-maven-plugin)?(:[^:]+)?:lock$").asMatchPredicate();
+    String LOCKER_DIR = ".locker";
     String LOCKER_POM_PATH = LOCKER_DIR + "/pom.xml";
     String POM_SHA_512_PATH = LOCKER_DIR + "/.last-pom-sha512";
 
-    String LOCKER_PROFILE = "mvnpm-locker";
+    String LOCKER_PROFILE = "locker";
 }
